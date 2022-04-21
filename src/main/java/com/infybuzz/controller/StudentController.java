3eeffb081547d7573f9adff17d2a6013831e9f71
Dario Caric <dario.caric@ericsson.com>
2022-04-21 21:42:03 +0200

    testing at push

--- src/main/java/com/infybuzz/controller/StudentController.java
+++ src/main/java/com/infybuzz/controller/StudentController.java
@@ -35,6 +35,7 @@ public class StudentController {
         return new ResponseEntity<>(studentService.getAllStudents2(), HttpStatus.OK);
     }
     */
+    
     public List<StudentSimple> getAllStudents2(@PathVariable String name) {
         return studentService.getAllStudents2(name);
     }
