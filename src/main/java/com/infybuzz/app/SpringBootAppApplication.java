97b5c850a3fe529e086add0144b3a21fab7d99fc
Dario Caric <dario.caric@ericsson.com>
2022-04-23 19:27:06 +0200

    introduced jtw

--- src/main/java/com/infybuzz/app/SpringBootAppApplication.java
+++ src/main/java/com/infybuzz/app/SpringBootAppApplication.java
@@ -5,11 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Component;
 
-@SpringBootApplication(scanBasePackages = "com.infybuzz")
+@SpringBootApplication(scanBasePackages = "com.infybuzz", exclude = { SecurityAutoConfiguration.class})
 @ComponentScan({"com.infybuzz.controller", "com.infybuzz.service"})
 @EntityScan("com.infybuzz.entity")
 @EnableJpaRepositories("com.infybuzz.repository")
